6e496b00fb2e54f4b70d26beccf4205c289f8abb
AhnHEYJI <dkwlgpwl11@naver.com>
2024-10-28 09:55:10 +0900

    시작메세지알림

---
+++ src/main/java/com/example/java_text_board/Main.java
@@ -0,0 +1,9 @@
+package com.example.java_text_board;
+
+public class Main {
+    public static  void  main(String[] args){
+        System.out.println("==[텍스트게시판]");
+        System.out.println("==게시판을 시작합니다.");
+        System.out.println("==게시판을 종료합니다.");
+    }
+}
