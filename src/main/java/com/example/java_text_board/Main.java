538eaf914dd710a19b17480918e18cca37e7d689
AhnHEYJI <dkwlgpwl11@naver.com>
2024-10-28 10:09:29 +0900

    명령어입력받기연습

--- src/main/java/com/example/java_text_board/Main.java
+++ src/main/java/com/example/java_text_board/Main.java
@@ -1,9 +1,19 @@
 package com.example.java_text_board;
 
+import java.util.Scanner;
+
 public class Main {
-    public static  void  main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("==[텍스트게시판]");
         System.out.println("==게시판을 시작합니다.");
+        System.out.print("명령어)");
+
+        String cmd = sc.nextLine();
+        System.out.println(String.format("입력받은 명령어: %s", cmd));
+
+        sc.close();
+
         System.out.println("==게시판을 종료합니다.");
     }
 }
